be85da39aa8d0185f5fa913e7f5a3ffe415a9c9a
pablo <pabloocampopita@gmail.com>
2019-07-18 14:44:23 -0400

    joey is co

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
 const express = require('express');
 const app = express();
 
+app.set("view engine", "ejs");
+
+app.use(express.static('./public'));
+
+app.get('/login', function (req, res) {
+  res.render('Login');
+});
+
+
+
 app.get('/Views/homepage.ejs', function (req, res) {
     res.render('homepage')
     console.log(err)
   })
 
-app.listen(3000, () =>
-  console.log('Example app listening on port 3000!'),
-);
+
+  app.listen(3000, function(err){
+    if (err){
+    console.log(err)
+    }
+    console.log('server is live on port 3000!!')
+})
