5b3afe7d4fb8d7950bc05d48a01f569a0793429f
carlitoes <2004carlito@gmail.com>
2019-07-22 10:51:00 -0400

    creating the database for Jungle including jobs, users, userjobs, parentchild, and childfavorites

---
+++ models/jobs.js
@@ -0,0 +1,40 @@
+// creating our task model
+// set it to export because our index.js file is expecting an 
+// export of a model
+module.exports = function(sequelize, DataTypes){
+    var Jobs = sequelize.define("Jobs", {
+        category: {
+            type: DataTypes.STRING, 
+            allowNull: false
+        },
+        pay: {
+            type: DataTypes.INTEGER, 
+            allowNull: false
+        },
+        address: {
+            type: DataTypes.STRING, 
+            allowNull: false
+        },
+        length: {
+            type: DataTypes.STRING, 
+            allowNull: false
+        },
+        title: {
+            type: DataTypes.STRING, 
+            allowNull: false
+        },
+        description: {
+            type: DataTypes.TEXT, 
+            allowNull: false
+        },
+        userId: {
+            type: DataTypes.INTEGER,
+        },
+        avatar: {
+            type: DataTypes.STRING, 
+            allowNull: false
+        }
+    });
+
+    return Jobs
+}
---
+++ models/parentChild.js
@@ -0,0 +1,17 @@
+// creating our task model
+// set it to export because our index.js file is expecting an 
+// export of a model
+module.exports = function(sequelize, DataTypes){
+    var parentChild = sequelize.define("parentChild", {
+        childId: {
+            type: DataTypes.INTEGER, 
+            allowNull: false
+        },
+        parentId: {
+            type: DataTypes.INTEGER, 
+            allowNull: false
+        }
+    });
+
+    return Jobs
+}
