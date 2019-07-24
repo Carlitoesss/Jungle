9402073cfa57133f4f6e1f4053c5052ea2314b56
Joseph <firepheonix1914@gmail.com>
2019-07-24 13:10:09 -0400

    resolving issue

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 const express = require('express');
 const app = express();
 const database = require('./models');
+const bodyParser = require('body-parser').urlencoded({extended: false})
+var session = require("express-session");
+
+// requiring passport as weve configured it
+var passport = require("./config/passport");
+
+// setting up all middleware
+app.use(bodyParser);
+app.use(session({ public: true, secret:"password", resave: true, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+
 
 app.set("view engine", "ejs");
 
@@ -18,7 +31,7 @@ app.get('/SignUp', function (req, res) {
   res.render('SignUp');
 });
 
-app.get('/home', function (req, res) {
+app.get('/homepage', function (req, res) {
     res.render('homepage')
     console.log(err)
   })
@@ -43,10 +56,11 @@ app.get('/profile', function (req, res) {
   res.render('profile')
 })
 
+
 database.sequelize.sync().then(function(){
-    app.listen(3000, function(err){
-        if (err)
-            console.log(err)
-        console.log('Server is live on port: ' + 3000)
-    })
+  app.listen(3000, function(err){
+      if (err)
+          console.log(err)
+      console.log('Server is live on port: 3000' )
+  })
 });
