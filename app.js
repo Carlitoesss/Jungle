e501abda91c1deb39a66d1e990b83b4db855f596
Joseph <firepheonix1914@gmail.com>
2019-07-29 10:29:48 -0400

    pushin hard coded job

--- app.js
+++ app.js
@@ -52,9 +52,9 @@ app.get('/singlejob', function (req, res) {
     res.render('singlejob');
 });
 
-// // app.get('/creatingjob', function (req, res) {
-// //     res.render('CreatingJob')
-// // })
+// app.get('/createjob', function (req, res) {
+//     res.render('createjob')
+// })
   
 app.get('/chat', function (req, res) {
   res.render('Chat')
